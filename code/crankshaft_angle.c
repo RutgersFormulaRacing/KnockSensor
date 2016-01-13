61dffdd43132ff6b4cce05cf6e59f78d3f279bf6
Rajan Patel <compgeek97@gmail.com>
2016-01-12 22:20:49 -0500

    set up registers

--- code/crankshaft_angle.c
+++ code/crankshaft_angle.c
@@ -3,9 +3,15 @@
 #include <stdlib.h>
 #include <avr/interrupt.h>
 
+uint8_t ticks = 0;
+uint8_t tmrOvf = 0;
 
 int main(void) {
+    TCCR0B = 0x02; //enable prescaler
+    TIMSK = 0x02; //enable overflow interrupts
+    DDRB = 0x10; //PB4 is an output
 
+    while(1){}
 }
 
 ISR(INT0_vect) {
