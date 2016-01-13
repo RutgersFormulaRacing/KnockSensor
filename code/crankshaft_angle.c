836ca0690e95fce01d844696488a94729728f211
Rajan Patel <compgeek97@gmail.com>
2016-01-12 23:14:53 -0500

    missed tooth detection

--- code/crankshaft_angle.c
+++ code/crankshaft_angle.c
@@ -3,8 +3,13 @@
 #include <stdlib.h>
 #include <avr/interrupt.h>
 
-uint8_t tooth = 0;
+uint8_t tooth = 1;
 uint8_t tmrOvf = 0;
+uint16_t prevPeriod = 0;
+
+uint16_t micros(void) {
+    return ((uint16_t)tmrOvf*255+TCNT0); //with prescaler 8, each tick is one microsecond
+}
 
 int main(void) {
     TCCR0B = 0x02; //set prescaler to 8
@@ -12,12 +17,11 @@ int main(void) {
     DDRB = 0x10; //set PB4 to output
 
     while(1) {
-
+        if(micros() > prevPeriod + (prevPeriod >> 1)) {
+            PORTB |= 0x10;
+            tooth = 1;
+        }
     }
-}
-
-uint16_t micros(void) {
-    return ((uint16_t)tmrOvf*255+TCNT0); //with prescaler 8, each tick is one microsecond
 }
 
 ISR(INT0_vect) {
