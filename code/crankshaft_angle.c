7386c5d821c694a3944e0f883c837e437bdd31fd
Rajan Patel <compgeek97@gmail.com>
2016-01-12 22:46:59 -0500

    interrupts

--- code/crankshaft_angle.c
+++ code/crankshaft_angle.c
@@ -3,17 +3,29 @@
 #include <stdlib.h>
 #include <avr/interrupt.h>
 
-uint8_t ticks = 0;
+uint8_t tooth = 0;
 uint8_t tmrOvf = 0;
 
 int main(void) {
-    TCCR0B = 0x02; //enable prescaler
+    TCCR0B = 0x02; //set prescaler to 8
     TIMSK = 0x02; //enable overflow interrupts
-    DDRB = 0x10; //PB4 is an output
+    DDRB = 0x10; //set PB4 to output
 
-    while(1){}
+    while(1) {
+
+    }
+}
+
+uint16_t micros(void) {
+    return ((uint16_t)tmrOvf*255+TCNT0); //with prescaler 8, each tick is one microsecond
 }
 
 ISR(INT0_vect) {
+    TCNT0 = 0;
+    tmrOvf = 0;
+    tooth++;
+}
 
+ISR( TIMER0_OVF_vect) {
+    tmrOvf++;
 }
