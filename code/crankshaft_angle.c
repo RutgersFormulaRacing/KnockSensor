f525715667e87a7bb406f513e51e6f110514b87a
Rajan Patel <rpatel3001@gmail.com>
2016-01-13 23:17:46 -0500

    actually enable global interrupts

--- code/crankshaft_angle.c
+++ code/crankshaft_angle.c
@@ -17,6 +17,8 @@ int main(void) {
     DDRB = 0x10; //set PB4 to output
     MCUCR |= 0x03; //set INT0/PB2 to trigger on rising edge
     GIMSK = 0x40; //enable INT0 interrupts
+    
+    sei(); //turn on interrupts globally
 
     while(1) {
         if(micros() > prevPeriod + (prevPeriod >> 1)) { //if it's been more than 1.5*the previous period, probably missed a tooth
