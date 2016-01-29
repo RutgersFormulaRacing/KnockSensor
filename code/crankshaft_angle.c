cf94f3aaf6efa16c2fadfd0e72ed4f61345eefb8
Rajan Patel <rpatel3001@gmail.com>
2016-01-29 16:29:24 -0500

    actually update prevPeriod

--- code/crankshaft_angle.c
+++ code/crankshaft_angle.c
@@ -1,7 +1,9 @@
-#define F_CPU 1000000
+#define F_CPU 1000000   //CKDIV8 fuse is set
 
 #include <stdlib.h>
 #include <avr/interrupt.h>
+
+#define TDC_TOOTH 1
 
 uint8_t tooth = 1;
 uint8_t tmrOvf = 0;
@@ -15,27 +17,28 @@ int main(void) {
     TCCR0B = 0x01; //set counter prescaler to 1
 		   //0x01 for prescaler=1, 0x02 for prescaler=8
     TIMSK = 0x02; //enable counter0 overflow interrupts
-    DDRB = 0x10; //set PB4 to output
+    DDRB = 0x10; //set PB4 as output
     MCUCR |= 0x03; //set INT0/PB2 to trigger an interrupt on every rising edge
     GIMSK = 0x40; //enable INT0 interrupts
     
     sei(); //turn on interrupts globally
 
     while(1) {
-        if(micros() > prevPeriod + (prevPeriod >> 1)) { //if it's been more than 1.5*the previous period, probably missed a tooth
+        if(micros() > (prevPeriod + (prevPeriod >> 1))) { //if it's been more than 1.5*the previous period, probably missed a tooth, and we're back at the beginning
             PORTB |= 0x10; //turn on pb4
             tooth = 1; //reset tooth count
         }
-        if(tooth == 3) {
+        if(tooth - TDC_TOOTH== 2) {	//we've reached the end of the "knock zone"
             PORTB &= 0xEF; //turn off pb4
         }
     }
 }
 
 ISR(INT0_vect) {
+    prevPeriod = micros();
     TCNT0 = 0;
-    tooth++;
     tmrOvf = 0;
+    tooth++;
 }
 
 ISR(TIMER0_OVF_vect) {
