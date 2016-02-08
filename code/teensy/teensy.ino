72b18564c72ad1047f634e285a906469ced38649
Rajan Patel <rpatel3001@gmail.com>
2016-02-08 13:03:52 -0500

    add pseudocode and make diag flash timing friendly

--- code/teensy/teensy.ino
+++ code/teensy/teensy.ino
@@ -14,7 +14,6 @@ const int vibration_signal_pin = A0;
 // GUItool: begin automatically generated code
 AudioInputAnalog         adc1;           //xy=137.1999969482422,188.1999969482422
 AudioAnalyzeToneDetect   tone1;          //xy=336.20001220703125,162.1999969482422
-AudioAnalyzePeak         peak1;          //xy=336.2000274658203,271.2000274658203
 AudioAnalyzeRMS          rms1;           //xy=337.2000274658203,322.2000274658203
 AudioAnalyzeFFT256       fft256_1;       //xy=338.1999969482422,109.19999694824219
 AudioAnalyzeNoteFrequency notefreq1;      //xy=342.2000274658203,218.20001220703125
@@ -23,10 +22,11 @@ AudioConnection          patchCord1(adc1, fft256_1);
 AudioConnection          patchCord2(adc1, tone1);
 AudioConnection          patchCord3(adc1, notefreq1);
 AudioConnection          patchCord4(adc1, fft1024_1);
-AudioConnection          patchCord5(adc1, peak1);
 AudioConnection          patchCord6(adc1, rms1);
 // GUItool: end automatically generated code
 
+//elapsedMillis emillis;
+//elapsedMicros emicros;
 
 void setup() {
   pinMode(diagLED_device_on, OUTPUT);
@@ -34,14 +34,22 @@ void setup() {
   pinMode(diagLED_in_knock_zone, OUTPUT);
   pinMode(diagLED_receiving_vibration_signal, OUTPUT);
   pinMode(vibration_signal_pin, INPUT);
-
-  digitalWrite(diagLED_device_on, HIGH);
 }
 
 void loop() {
-  digitalWrite(diagLED_device_on, HIGH);   // set the LED on
-  delay(1000);                  // wait for a second
-  digitalWrite(diagLED_device_on, LOW);    // set the LED off
-  delay(1000);                  // wait for a second
+  //blink the diagnostic LED at 1 hz to show that our code is running on the device
+  if(millis() / 500 % 2 == 0) {
+    digitalWrite(diagLED_device_on, HIGH);
+  } else {
+    digitalWrite(diagLED_device_on, LOW);
+  }
+
+  /*
+   * if not in knock zone
+   *  detect and save baseline signal (rms? mean? how many bins? digital filter or analog?)
+   * else
+   *  if signal - baseline > threshold (how high is threshold?)
+   *    knock is detected
+   */
 }
 
